df3fb2475ccceaa19d96e081a884c6568284b612
Jen Lim <jennolim@gmail.com>
2013-05-28 16:59:23 -0400

    Start Rails, install Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MentorConnect::Application.config.secret_token = '730c5d33d15cf0d586515ce896640e608cdf7b80a845e3bd35d186d76663cc9a43ecc236ae2eae006d653d05d8ad88cc8f1ab37e4c27b04f89bc790b4beceb35'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+MentorConnect::Application.routes.draw do
+  devise_for :users
+
+  root :to => "home#index"
+end
