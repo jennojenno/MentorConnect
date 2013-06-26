72f23a8edf8be0718821fd3b670b0c20b1f2cd0f
Jen Lim <jennolim@gmail.com>
2013-06-26 15:54:18 -0400

    1.9.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# wifiread = ActiveSupport::JSON.decode(File.read('public/manhattanwifi.json')) 
+# wifiread["data"].each do |a| 
+#   Wifi.create!(:name => a[10], 
+#     :location => a[9], 
+#     :address => a[15][0])
+# end 
+
+Rails.application.eager_load! # To load all models app/models/**/*.rb
+
+all_records = ActiveRecord::Base.descendants.map &:all
+
+mydata = all_records.to_json
+binding.pry
