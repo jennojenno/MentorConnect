1bf61d7c7811131bd436ab6482ed570d210590ae
Jen Lim <jennolim@gmail.com>
2013-06-27 11:13:52 -0400

    Users can add courses seamlessly

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-  attr_accessible :category, :title, :dates, :online, :in_person, :user_id
+  attr_accessible :category, :desc, :title, :dates, :online, :in_person, :user_id
 
   belongs_to :user
   has_many :favorites
